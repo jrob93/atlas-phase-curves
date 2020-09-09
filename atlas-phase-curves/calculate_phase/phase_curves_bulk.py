a519800cbc0b631b2391d7a077fc5cc6a945ab03
jrob93 <jrobinson@mp131325.home>
2020-09-09 12:15:58 +0100

    parallelise

--- atlas-phase-curves/calculate_phase/phase_curves_bulk.py
+++ atlas-phase-curves/calculate_phase/phase_curves_bulk.py
@@ -1,7 +1,16 @@
 import pandas as pd
 import mysql.connector
-import subprocess
 import numpy as np
+import time
+import multiprocessing
+from multiprocessing import Pool
+import subprocess
+
+def phase_fit_func(mpc_number):
+    py_cmd="python sbpy_phase_fit_data_clip_push.py -n {}".format(mpc_number)
+    print(py_cmd)
+    subprocess.Popen(py_cmd,shell=True).wait()
+    return
 
 """
 # connect to the database
@@ -27,40 +36,45 @@ mpc_number_list=np.random.choice(df['mpc_number'].astype(int),10,replace=False)
 print(mpc_number_list)
 """
 
-# mpc_number_list=[339933, 370192, 416678, 238537, 181325, 358275,  68172, 383854,  41777, 215944]
-mpc_number_list=[339933, 370192, 416678, 238537]#, 181325, 358275,  68172, 383854,  41777, 215944]
+mpc_number_list=[339933, 370192, 416678, 238537, 181325, 358275,  68172, 383854,  41777, 215944]
+# mpc_number_list=[339933, 370192, 416678, 238537]
+print(mpc_number_list)
 
-# cmds=[]
-for N_mpc in mpc_number_list:
-    cmd="python sbpy_phase_fit_data_clip_push.py -n {}".format(N_mpc)
-    print(cmd)
-    # cmds.append(cmd)
-    subprocess.Popen(cmd,shell=True).wait()
-    # exit()
+# add mpc_number or date requirements to run?
+# run in reverse date order so that objects that have not been calculated are done first?
 
-#     # exit()
-# # exit()
-# print(cmds)
-#
-# import multiprocessing
-# import subprocess
-#
-# def work(cmd):
-#     return subprocess.Popen(cmd, shell=True)
-#
-# if __name__ == '__main__':
-#     count = multiprocessing.cpu_count()
-#     pool = multiprocessing.Pool(processes=count)
-#     print(count, pool)
-#     pool.map(work, cmds)
-
-# # set up pool and find orbits
-# # works best if the command is functionalised!
+# print("run in serial")
 # t_start=time.time()
-# pool = Pool(threads)
-# multiple_results = [pool.apply_async(bin_orb_func, args=("/".join(d.split("/")[:-1]),save_path,d.split("/")[-1])) for d in all_run_dirs]
-# pool.close()
-# pool.join()
+# for n in mpc_number_list:
+#     phase_fit_func(n)
 # t_end=time.time()
-# print "N_jobs = {}".format(len(all_run_dirs))
-# print "t_pool = {}".format(t_end-t_start)
+# print ("N_jobs = {}".format(len(mpc_number_list)))
+# print ("t_pool = {}".format(t_end-t_start))
+
+# set up pool and fit phase
+# threads=4
+threads=multiprocessing.cpu_count()
+
+jobs_done=0
+t_start=time.time()
+while len(mpc_number_list)>0:
+    sub_list=mpc_number_list[:threads]
+    mpc_number_list=mpc_number_list[threads:]
+
+    print("run parallel, {} threads".format(threads))
+    # t_start=time.time()
+    pool = Pool(threads)
+    multiple_results = [pool.apply_async(phase_fit_func, args=(n,)) for n in sub_list]
+    pool.close()
+    pool.join()
+    t_end=time.time()
+    print()
+    # print("N_jobs = {}".format(len(sub_list)))
+    # print("t_pool = {}".format(t_end-t_start))
+    jobs_done+=len(sub_list)
+    t_elapsed=t_end-t_start
+    print("N jobs done = {}".format(jobs_done))
+    print("time elapsed = {}".format(t_elapsed))
+    print("{} objects/second".format(float(jobs_done)/t_elapsed))
+
+    print()
