43a96d06da568dcf34b23886cf08bb0d1a20f0ce
jrob93 <jrobinson@mp131325.home>
2021-01-10 17:23:22 +0000

    add orbid qry

--- atlas-phase-curves/calculate_phase/atlas_SQL_query_df.py
+++ atlas-phase-curves/calculate_phase/atlas_SQL_query_df.py
@@ -16,15 +16,176 @@ This function version returns the dataframe
 
 from pandas import read_sql_query
 
-def atlas_SQL_query(cnx,mpc_number=4986,filter="all"):
+def get_orb_elements_id(cnx,mpc_number=False,name=False):
+    """ Function to retrieve the orbital_elements_id from either mpc_number or name.
+     name case should not matter for SQL """
 
+    print(mpc_number,name)
+
+    # use mpc_number or name to get orbital_elements_id
+    if name:
+        qry="SELECT orbital_elements_id FROM atlas_objects WHERE name=\"{}\";".format(name)
+    else:
+        qry="SELECT orbital_elements_id FROM atlas_objects WHERE mpc_number=\"{}\";".format(mpc_number)
+
+    cursor=cnx.cursor()
+    cursor.execute(qry)
+    row = cursor.fetchone()
+    orbital_elements_id=row[0]
+
+    print(orbital_elements_id)
+
+    return orbital_elements_id
+
+def atlas_SQL_query(cnx,orbital_elements_id,filter="all"):
+    """ Query that accept orb orbital_elements_id """
+
+    # # use mpc_number or name to get orbital_elements_id
+    # if name:
+    #     qry="SELECT orbital_elements_id FROM atlas_objects WHERE name=\"{}\";".format(name)
+    # else:
+    #     qry="SELECT orbital_elements_id FROM atlas_objects WHERE mpc_number=\"{}\";".format(mpc_number)
+    #
+    # cursor=cnx.cursor()
+    # cursor.execute(qry)
+    # row = cursor.fetchone()
+    # orbital_elements_id=row[0]
+    #
     # print(cnx)
     # orbital_elements_id=mpc_number
 
     # write out the sql query
 
     # REVIEW THIS QUERY, E.G. WHY d.expname?
+    # remove unneccesary data, e.g. apparent_mag
+
     if filter=="all":
+
+        # sqlQuery_alan = u"""
+        # SELECT d.mjd, d.m,  d.dfitmag as merr, a.filter, o.observer_distance, o.heliocentric_distance, o.phase_angle, d.m - 5*log10(o.heliocentric_distance*o.observer_distance) as reduced_mag, o.apparent_mag, o.galactic_latitude
+        # FROM
+        #     dophot_photometry d,
+        #     orbfit_positions o,
+        #     atlas_exposures a
+        # WHERE
+        #     d.orbfit_postions_id = o.primaryId
+        #         AND a.expname = d.expname
+        #         AND o.mpc_number = %(mpc_number)s;
+        # """ % locals()
+
+        # based on dave's query that uses orbital_elements_id
+        sqlQuery_dave = u"""
+        SELECT d.mjd, d.m,  d.dfitmag as merr, a.filter, o.observer_distance, o.heliocentric_distance, o.phase_angle, d.m - 5*log10(o.heliocentric_distance*o.observer_distance) as reduced_mag, o.apparent_mag, o.galactic_latitude
+        FROM
+            dophot_photometry d,
+            orbfit_positions o,
+            atlas_exposures a
+        WHERE
+            d.orbfit_postions_id = o.primaryId
+                AND a.expname = d.expname
+                AND o.orbital_elements_id = %(orbital_elements_id)s;
+        """ % locals()
+
+    else:
+        # sqlQuery_alan = u"""
+        # SELECT d.mjd, d.m,  d.dfitmag as merr, a.filter, o.observer_distance, o.heliocentric_distance, o.phase_angle, d.m - 5*log10(o.heliocentric_distance*o.observer_distance) as reduced_mag, o.apparent_mag, o.galactic_latitude
+        # FROM
+        #     dophot_photometry d,
+        #     orbfit_positions o,
+        #     atlas_exposures a
+        # WHERE
+        #     filter = '%(filter)s'
+        #         AND d.orbfit_postions_id = o.primaryId
+        #         AND a.expname = d.expname
+        #         AND o.mpc_number = %(mpc_number)s;
+        # """ % locals()
+
+        sqlQuery_dave = u"""
+        SELECT d.mjd, d.m,  d.dfitmag as merr, a.filter, o.observer_distance, o.heliocentric_distance, o.phase_angle, d.m - 5*log10(o.heliocentric_distance*o.observer_distance) as reduced_mag, o.apparent_mag, o.galactic_latitude
+        FROM
+            dophot_photometry d,
+            orbfit_positions o,
+            atlas_exposures a
+        WHERE
+            filter = '%(filter)s'
+                AND d.orbfit_postions_id = o.primaryId
+                AND a.expname = d.expname
+                AND o.orbital_elements_id = %(orbital_elements_id)s;
+        """ % locals()
+
+    # perform the query and store results as a dataframe
+    # sqlQuery=sqlQuery_alan
+    sqlQuery=sqlQuery_dave
+    print(sqlQuery)
+    # df=pd.read_sql_query(sqlQuery,cnx)
+    df=read_sql_query(sqlQuery,cnx)
+
+    # cnx.close()
+
+    return df # should also return orbital_elements_id!!!
+
+def atlas_SQL_query_test(cnx,mpc_number=False,filter="all",name=False):
+    """ Test grabbing orb id and the data in the same function """
+
+    # use mpc_number or name to get orbital_elements_id
+    if name:
+        qry="SELECT orbital_elements_id FROM atlas_objects WHERE name=\"{}\";".format(name)
+    else:
+        qry="SELECT orbital_elements_id FROM atlas_objects WHERE mpc_number=\"{}\";".format(mpc_number)
+
+    cursor=cnx.cursor()
+    cursor.execute(qry)
+    row = cursor.fetchone()
+    orbital_elements_id=row[0]
+
+    print(cnx)
+
+    if filter=="all":
+
+        # based on dave's query that uses orbital_elements_id
+        sqlQuery_dave = u"""
+        SELECT d.mjd, d.m,  d.dfitmag as merr, a.filter, o.observer_distance, o.heliocentric_distance, o.phase_angle, d.m - 5*log10(o.heliocentric_distance*o.observer_distance) as reduced_mag, o.apparent_mag, o.galactic_latitude
+        FROM
+            dophot_photometry d,
+            orbfit_positions o,
+            atlas_exposures a
+        WHERE
+            d.orbfit_postions_id = o.primaryId
+                AND a.expname = d.expname
+                AND o.orbital_elements_id = %(orbital_elements_id)s;
+        """ % locals()
+
+    else:
+
+        sqlQuery_dave = u"""
+        SELECT d.mjd, d.m,  d.dfitmag as merr, a.filter, o.observer_distance, o.heliocentric_distance, o.phase_angle, d.m - 5*log10(o.heliocentric_distance*o.observer_distance) as reduced_mag, o.apparent_mag, o.galactic_latitude
+        FROM
+            dophot_photometry d,
+            orbfit_positions o,
+            atlas_exposures a
+        WHERE
+            filter = '%(filter)s'
+                AND d.orbfit_postions_id = o.primaryId
+                AND a.expname = d.expname
+                AND o.orbital_elements_id = %(orbital_elements_id)s;
+        """ % locals()
+
+    # perform the query and store results as a dataframe
+    # sqlQuery=sqlQuery_alan
+    sqlQuery=sqlQuery_dave
+    print(sqlQuery)
+    # df=pd.read_sql_query(sqlQuery,cnx)
+    df=read_sql_query(sqlQuery,cnx)
+
+    # cnx.close()
+
+    return df # should also return orbital_elements_id!!!
+
+def atlas_SQL_query_orig(cnx,mpc_number=4986,filter="all"):
+    """ Original query that searches by mpc number """
+
+    if filter=="all":
+
         sqlQuery_alan = u"""
         SELECT d.mjd, d.m,  d.dfitmag as merr, a.filter, o.observer_distance, o.heliocentric_distance, o.phase_angle, d.m - 5*log10(o.heliocentric_distance*o.observer_distance) as reduced_mag, o.apparent_mag, o.galactic_latitude
         FROM
@@ -36,6 +197,7 @@ def atlas_SQL_query(cnx,mpc_number=4986,filter="all"):
                 AND a.expname = d.expname
                 AND o.mpc_number = %(mpc_number)s;
         """ % locals()
+
     else:
         sqlQuery_alan = u"""
         SELECT d.mjd, d.m,  d.dfitmag as merr, a.filter, o.observer_distance, o.heliocentric_distance, o.phase_angle, d.m - 5*log10(o.heliocentric_distance*o.observer_distance) as reduced_mag, o.apparent_mag, o.galactic_latitude
@@ -52,12 +214,13 @@ def atlas_SQL_query(cnx,mpc_number=4986,filter="all"):
 
     # perform the query and store results as a dataframe
     sqlQuery=sqlQuery_alan
+    print(sqlQuery)
     # df=pd.read_sql_query(sqlQuery,cnx)
     df=read_sql_query(sqlQuery,cnx)
 
     # cnx.close()
 
-    return df
+    return df # should also return orbital_elements_id!!!
 
 # if __name__ == "__main__":
 #
