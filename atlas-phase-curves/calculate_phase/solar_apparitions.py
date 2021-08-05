a38b2110241acb0be48904614dd904c385d0da0f
jrob93 <jrobinson72@qub.ac.uk>
2021-08-05 15:40:35 +0100

    add synodic period options

--- atlas-phase-curves/calculate_phase/solar_apparitions.py
+++ atlas-phase-curves/calculate_phase/solar_apparitions.py
@@ -8,8 +8,6 @@ Delete the loaded files to get fresh ones
 """
 
 import pandas as pd
-from calculate_phase import atlas_SQL_query_df
-from calculate_phase import atlas_database_connection
 from optparse import OptionParser
 import time
 import numpy as np
@@ -18,36 +16,51 @@ from astroquery.jplhorizons import Horizons
 import os
 from scipy.signal import argrelextrema
 
+# importing our classes depends on whether this file as a script or module
+if __name__ == "__main__": # import other modules as a script
+    import atlas_SQL_query_df
+    import atlas_database_connection
+else: # import as a package
+    import calculate_phase.atlas_SQL_query_df as atlas_SQL_query_df
+    import calculate_phase.atlas_database_connection as atlas_database_connection
+
 class solar_apparitions():
     # fixed parameters
     loc = "T05" # which telescope to use for Horizons query, T05 or T08?
     mjd_jd = 2400000.5 # conversion from MJD to JD
+    # G = 6.693e-11 # Newton's Gravitational Constant, m3⋅kg−1⋅s−2
+    # M_sun = # Mass of the sun
 
     def __init__(self,
         mpc_number=False,
         name=False,
+        df_data=None,
         reload=False,
         save_path=False,
         elong_mod = 360,
-        data_load_path="/Users/jrobinson/atlas-phase-curves/atlas-phase-curves/results_analysis/obs",
-        eph_load_path="solar_apparitions_files"):
+        data_load_path=None,
+        eph_load_path=None):
 
-        # set up data path names
+        # path to save a figure
+        self.save_path = save_path
+
+        # object identifiers
         if name:
             self.obj_id = name
             self.obj_id_save = "_".join(name.split())
         else:
             self.obj_id = mpc_number
             self.obj_id_save = mpc_number
+
+        # set up data path names
+        self.data_load_path = data_load_path
+        self.eph_load_path = eph_load_path
         self.data_fname = "{}/df_data_{}.csv".format(data_load_path,self.obj_id_save)
         self.eph_fname = "{}/df_eph_{}.csv".format(eph_load_path,self.obj_id_save)
         print(self.data_fname)
         print(self.eph_fname)
 
-        # path to save a figure
-        self.save_path = save_path
-
-        # delete existing data to force refresh
+        # Option to delete existing data files
         if reload:
             for f in [self.data_fname,self.eph_fname]:
                 try:
@@ -56,25 +69,34 @@ class solar_apparitions():
                 except:
                     continue
 
-        # Try load an existing data file
-        if os.path.isfile(self.data_fname):
-            print("load data")
-            df_data = pd.read_csv(self.data_fname,index_col=0)
+        # Check if dataframe has been passed, otherwise load
+        if df_data is None:
+
+            # Try load an existing data file
+            if os.path.isfile(self.data_fname):
+                print("load data")
+                df_data = pd.read_csv(self.data_fname,index_col=0)
+            else:
+                print("query data")
+                # connect to database
+                cnx=atlas_database_connection.database_connection().connect()
+
+                # get orbit_id
+                orbid=atlas_SQL_query_df.get_orb_elements_id(cnx,mpc_number,name)
+
+                # do the query
+                df_data=atlas_SQL_query_df.atlas_SQL_query_orbid_expname(cnx,orbid)
+
+                # save df
+                if data_load_path is not None:
+                    print("save {}".format(self.data_fname))
+                    df_data.to_csv(self.data_fname)
         else:
-            print("query data")
-            # connect to database
-            cnx=atlas_database_connection.database_connection().connect()
+            print("use passed df_data")
 
-            # get orbit_id
-            orbid=atlas_SQL_query_df.get_orb_elements_id(cnx,mpc_number,name)
-
-            # do the query
-            df_data=atlas_SQL_query_df.atlas_SQL_query_orbid_expname(cnx,orbid)
-
-            # save df
-            df_data.to_csv(data_fname)
-
-        # ADD OPTION TO PASS DATAFRAME INSTEAD OF LOADING
+        # print(df_data)
+        print(np.median(df_data["reduced_mag"]))
+        print(np.median(df_data["merr"]))
 
         # drop any nans and sort by date
         df_data = df_data.dropna()
@@ -92,7 +114,10 @@ class solar_apparitions():
             self.ydata=np.array(df_data["sun_obs_target_angle"])
             self.ydata_lab="solar elongation"
 
-    def solar_elongation_JPL(self):
+        # initialise ephemerides dataframe
+        self.df_eph = None
+
+    def solar_elongation_JPL(self,JPL_step='1d'):
 
         # Get the JPL Horizons ephemerides to search for the turning points of solar elongation
         # This is a relatively slow check, only do this for NEAs
@@ -102,20 +127,37 @@ class solar_apparitions():
             df_eph = pd.read_csv(self.eph_fname,index_col=0)
         else:
             print("query Horizons")
-            t1=Time(int(np.amin(df_data["mjd"])),format="mjd")
-            t2=Time(int(np.amax(df_data["mjd"])),format="mjd")
-            epoch_list = {'start':t1.iso, 'stop':t2.iso, 'step':'1d'} # a range of epochs in Horizons format is FAST!
-            obj = Horizons(id=self.obj_id, location=self.loc, epochs=self.epoch_list)
+            t1=Time(int(np.amin(self.df_data["mjd"])),format="mjd")
+            t2=Time(int(np.amax(self.df_data["mjd"])),format="mjd")
+
+            if t1==t2:
+                print("dates equal, just use upper and lower mjds")
+                # add the first and last mjds in the dataset to fully bound all apparitions
+                turning_points = np.array([])
+                turning_points = np.insert(turning_points,0,self.xdata[0])
+                turning_points = np.insert(turning_points,len(turning_points),self.xdata[-1])
+                return np.array(turning_points)
+
+            epoch_list = {'start':t1.iso, 'stop':t2.iso, 'step':JPL_step} # a range of epochs in Horizons format is FAST!
+            # print(epoch_list)
+            obj = Horizons(id=self.obj_id, location=self.loc, epochs=epoch_list)
             eph = obj.ephemerides()
             df_eph = eph.to_pandas()
             df_eph["mjd"] = df_eph["datetime_jd"] - self.mjd_jd
             # save df
-            df_eph.to_csv(eph_fname)
+            if self.eph_load_path is not None:
+                print("save {}".format(self.eph_fname))
+                df_eph.to_csv(self.eph_fname)
 
         df_eph = df_eph.sort_values("mjd")
+        self.df_eph = df_eph
 
         # find the minima from JPL data
         elon_min_mjd = np.array(df_eph.iloc[argrelextrema(np.array(df_eph["elong"]), np.less)[0]]["mjd"])
+
+        # NOTE that searching for minima will not work when less than one apparition has been observed
+        # if time_span<synodic_period/2.0:
+            # elon_min_mjd = np.array([])
 
         # select by the exact JPL ephemerides
         turning_points = elon_min_mjd
@@ -125,21 +167,64 @@ class solar_apparitions():
         turning_points = np.insert(turning_points,len(turning_points),self.xdata[-1])
         # print(turning_points)
         # N_app = len(turning_points)-1
-        return np.array(turning_points)
+        turning_points = np.array(turning_points)
 
-    def solar_elongation(self,sol_elong_diff):
+        # ONLY COUNT APPARITIONS WITH DATA! see e.g. Kridsadaporn
+        # MAKE THIS FASTER, LIST comprehension?
+        n_data_app = []
+        for i in range(1,len(turning_points)):
+            # print(i-1,i)
+            n_data_app.append(len(self.xdata[(self.xdata>=turning_points[i-1]) &
+            (self.xdata<=turning_points[i])])) # equal on upper and lower bounds to catch cases with one detection e.g. Erinleeryan
+        n_data_app = np.array(n_data_app)
+        # print(n_data_app)
+        # print(turning_points[1:][n_data_app>0])
+        turning_points = np.insert(turning_points[1:][n_data_app>0],0,turning_points[0])
+        return turning_points
+
+    def solar_elongation(self,sol_elong_diff,period=False):
         # find the turning points of the solar elongation data
         # by searching for drops in the solar_elongation angle (which is generally monotonic when %360)
         ydiff = np.diff(self.ydata)
 
+        # we can check based on gaps in time as well as drops in elongation
+        if period:
+            xdiff = np.diff(self.xdata)
+            synodic_period = (period / ((period / 1.0) - 1.0))*365.0 # CHECK THIS https://en.wikipedia.org/wiki/Elongation_(astronomy)
+            print("synodic period = {}".format(synodic_period))
+            # mask = (ydiff<sol_elong_diff) | (xdiff>synodic_period)
+            # mask = (ydiff<sol_elong_diff) & (xdiff>synodic_period/2.0)
+
+            # mask = (ydiff<sol_elong_diff) | (xdiff>synodic_period/2.0)
+            mask = ((ydiff<sol_elong_diff) & (xdiff>14.0)) | (xdiff>synodic_period/2.0)
+
+        else:
+            mask = (ydiff<sol_elong_diff)
+
         # select only drops of at least a certain size
-        turning_points = self.xdata[1:][ydiff<sol_elong_diff]
+        turning_points = self.xdata[1:][mask]
+
+        # # IF PERIOD
+        # # reject any turning points that have a time gap shorter than period
+        # if period and (len(turning_points)>0):
+        #     print(turning_points)
+        #     print(np.diff(turning_points))
+        #     mask_xdiff = list(np.diff(turning_points)<synodic_period)
+        #     # print(mask_xdiff)
+        #     indices = np.array(range(len(turning_points)))
+        #     # print(indices)
+        #     # print(indices[1:])
+        #     drop = indices[1:][mask_xdiff]
+        #     print(drop)
+        #     turning_points = np.delete(turning_points,drop)
 
         # add the first and last mjds in the dataset to fully bound all apparitions
         turning_points = np.insert(turning_points,0,self.xdata[0])
         turning_points = np.insert(turning_points,len(turning_points),self.xdata[-1])
-        # print(turning_points)
+        print(turning_points)
         # N_app = len(turning_points)-1
+
+
         return np.array(turning_points)
 
     def plot_solar_elongation(self,turning_points=[]):
@@ -156,13 +241,20 @@ class solar_apparitions():
         gs = gridspec.GridSpec(1, 1)
         ax1 = plt.subplot(gs[0,0])
 
-        ax2 = ax1.twinx()
-        ax2.scatter(self.df_data["mjd"],self.df_data["reduced_mag"])
-        ax2.set_ylabel("reduced_mag")
+        # plot the solar elongation
+        ax1.scatter(self.df_data["mjd"],np.absolute(self.df_data["sun_obs_target_angle"]),
+        c="k",s=5,
+        label="solar elongation")
 
-        ax1.scatter(self.xdata,self.ydata,c="k",s=5,label=self.ydata_lab)
+        # plot the modded elongation angle
+        ax1.scatter(self.xdata,self.ydata,c="b",s=5,label=self.ydata_lab)
         ax1.set_xlabel("mjd")
         ax1.set_ylabel("angle(degrees)")
+
+        if self.df_eph is not None:
+            ax1.scatter(self.df_eph["mjd"],self.df_eph["elong"],
+            c="r",s=5,alpha=0.5,zorder=0,
+            label="solar elongation JPL")
 
         ax1.legend()
 
@@ -172,6 +264,14 @@ class solar_apparitions():
         title = "{}_{}".format("solar_apparitions",self.obj_id_save)
         fig.suptitle(title)
         plt.tight_layout()
+
+        # ax2 = ax1.twinx()
+        # ax2.scatter(self.df_data["mjd"],self.df_data["reduced_mag"])
+        # ax2.set_ylabel("reduced_mag")
+        #
+        # # Try reorder axes
+        # ax1.set_zorder(ax2.get_zorder()+1)
+        # ax1.patch.set_visible(False)
 
         if self.save_path:
             fname="{}/{}.png".format(self.save_path,title)
@@ -185,8 +285,42 @@ class solar_apparitions():
 
 if __name__ == "__main__":
 
-    sol = solar_apparitions(mpc_number=4986)
-    tp1 = sol.solar_elongation_JPL()
+    # mpc_number=4986
+    # mpc_number=3103
+    # name=False
+    mpc_number = False
+
+    # objects where ydiff does not work
+    name = "Asher"
+    period = 2.58 # yrs
+    # name = "2002 CW205"
+    # period = 3.59
+    # name = "2001 UG"
+    # period = 3.26
+    # name = "2008 RQ98"
+    # period = 4.19
+    # name = "2002 XQ7"
+    # period = 4.27
+    # name = "2003 OH12"
+    # period = 5.56
+    # name = "2001 TG99"
+    # period = 7.94
+
+    # Load/query data
+    # sol = solar_apparitions(mpc_number=mpc_number,name=name)
+    data_load_path="/Users/jrobinson/atlas-phase-curves/atlas-phase-curves/results_analysis/obs"
+    eph_load_path="/Users/jrobinson/atlas-phase-curves/atlas-phase-curves/solar_apparitions_files"
+    sol = solar_apparitions(mpc_number=mpc_number,name=name,data_load_path=data_load_path,eph_load_path=eph_load_path)
+    tp1 = sol.solar_elongation_JPL(JPL_step="7d")
+    print(tp1)
     tp2 = sol.solar_elongation(-1.0)
+    # tp2 = sol.solar_elongation(-1.0,period=period)
+    print(tp2)
     # sol.plot_solar_elongation(tp1)
     sol.plot_solar_elongation(tp2)
+    print(len(tp1)-1,len(tp2)-1)
+
+    # # Pass data directly, otherwise the code will load/query
+    # data_fname = "/Users/jrobinson/atlas-phase-curves/atlas-phase-curves/results_analysis/obs/df_data_4986.csv"
+    # df_data = pd.read_csv(data_fname,index_col=0)
+    # sol = solar_apparitions(mpc_number=mpc_number,df_data=df_data)
