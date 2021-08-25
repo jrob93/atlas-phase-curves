fe84bd05b476a266e58e7a8d676d9b0d0e55a8c8
jrob93 <jrobinson72@qub.ac.uk>
2021-08-25 11:45:34 +0100

    look at new data coming in

--- atlas-phase-curves/plot_obs.py
+++ atlas-phase-curves/plot_obs.py
@@ -3,40 +3,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import astropy.units as u
-from calculate_phase.atlas_SQL_query_df import atlas_SQL_query,get_orb_elements_id,get_unique_ids,atlas_SQL_query_orbid
+from calculate_phase.atlas_SQL_query_df import get_orb_elements_id,atlas_SQL_query_orbid
+from calculate_phase import atlas_database_connection
+from astropy.time import Time
+from datetime import datetime
 
-# data = pd.read_csv("results_analysis/obs/df_data_168576.csv",index_col=0)
-# data = pd.read_csv("results_analysis/obs/df_data_150621.csv",index_col=0)
-# data = pd.read_csv("results_analysis/obs/df_data_141570.csv",index_col=0)
-# data = pd.read_csv("results_analysis/obs/df_data_134649.csv",index_col=0)
-data = pd.read_csv("results_analysis/obs/df_data_17707.csv",index_col=0)
+mjd_lim = 59259 # 2021-02-14 00:00:00.000
+today = Time(datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")).mjd
+
+mpc_number = False
+name = "Pandion"
+
+# connect to database
+cnx=atlas_database_connection.database_connection().connect()
+
+# get data
+orbid=get_orb_elements_id(cnx,mpc_number,name)
+data_all_filt=atlas_SQL_query_orbid(cnx,orbid)
+data = data_all_filt
 print(data)
-# exit()
 
 time=np.array(data["mjd"])
 alpha = np.array(data['phase_angle'])# * u.deg
 mag = np.array(data["reduced_mag"])# * u.mag
 mag_err = np.array(data["merr"])# * u.mag
 
-mag_med_cut=5
-mag_med = np.nanmedian(mag)
-# mag=mag-mag_med
-mask=np.absolute(np.array(mag-mag_med))>mag_med_cut
-# print(mask)
-
 fig = plt.figure()
-gs = gridspec.GridSpec(1,1)
+gs = gridspec.GridSpec(2,1)
 ax1 = plt.subplot(gs[0,0])
+ax2 = plt.subplot(gs[1,0])
 
-fig.set_size_inches(20,12)
+# fig.set_size_inches(20,12)
 
 s1=ax1.scatter(alpha,mag,c=time,s=10,label="data time")
 ax1.errorbar(np.array(alpha),np.array(mag),np.array(mag_err),fmt='k.',alpha=0.2,label="data error",zorder=0,markersize="2")
 fig.colorbar(s1, ax=ax1,label="MJD")
 
-ax1.axhline(mag_med)
-ax1.axhline(mag_med-mag_med_cut)
-ax1.scatter(alpha[mask],mag[mask],c='r',marker="x")
+ax2.scatter(time,mag,s=10)
+ax2.axvline(mjd_lim)
+ax2.axvline(today)
+
+# # cut mag
+# mag_med_cut=5
+# mag_med = np.nanmedian(mag)
+# mask=np.absolute(np.array(mag-mag_med))>mag_med_cut
+# ax1.axhline(mag_med)
+# ax1.axhline(mag_med-mag_med_cut)
+# ax1.scatter(alpha[mask],mag[mask],c='r',marker="x")
 
 ax1.set_xlabel('alpha(degrees)')
 ax1.set_ylabel('mag')
